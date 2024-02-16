eb5cb038a97184e67a3916208a0ef2c808eba267
aditya-singh-2002 <adityasinghdhs@gmail.com>
2024-02-16 11:23:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/beerapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.beerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem.toString()
+            val beerList = getBeers(color)
+            val beers = beerList.joinToString("\n")
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = beers
+        }
+    }
+
+    private fun getBeers(color: String): List<String> {
+        return when (color) {
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
+        }
+    }
+}
